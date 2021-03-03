67ad3a04b3f66ed8c0560eca2a9154bf531784fd
aminriqky <aminriqky@gmail.com>
2021-03-03 19:37:38 +0700

    Update Telegram.js

--- Tugas 10 - React Native Intro/SanberApp/Tugas/Tugas12/Telegram.js
+++ Tugas 10 - React Native Intro/SanberApp/Tugas/Tugas12/Telegram.js
@@ -8,19 +8,24 @@ export default function Telegram() {
     }, [])
 
     return (
-        <View>
-            <View>
-                <View>
-                    <View>
-                        <Image/>
-                        <Text></Text>
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.subHeader}>
+                    <View style={styles.subContentHeader}>
+                        <Image
+                            style={styles.avatarDrawwer}
+                            source={require('./asset/drawwer.png')}
+                        />
+                        <Text style={styles.titleName}>Telegram</Text>
                     </View>
                     <View>
-                        <Image/>
+                        <Image 
+                            style={styles.avatarDrawwer} 
+                            source={require('./asset/search.png')}/>
                     </View>
                 </View>
             </View>
-            <View>
+            <View style={styles.content}>
                 <SafeAreaView>
                     <FlatList
                         data={Data}
@@ -28,24 +33,105 @@ export default function Telegram() {
                         renderItem={({item}) => {
                             return(
                                 <>
-                                <TouchableOpacity>
-                                    <View>
-                                        <Image/>
-                                        <View>
-                                            <Text></Text>
-                                            <Text></Text>
+                                <TouchableOpacity style={styles.messageContainer}>
+                                    <View style={{flexDirection: 'row'}}>
+                                        <Image
+                                            style={{height: 50, width: 50, borderRadius: 25}}
+                                            source={item.image}
+                                        />
+                                        <View style={styles.nameMessage}>
+                                            <Text>{item.name}</Text>
+                                            <Text>{item.message}</Text>
                                         </View>
                                     </View>
-                                    <View>
-                                        
+                                    <View style={{alignItems: 'center'}}>
+                                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                                            <Image style={{height: 8, width : 12, marginRight: 5}}
+                                            source={require('./asset/check.png')}/>
+                                            <Text>{item.time}</Text>
+                                        </View>
+                                        <View style={{
+                                            backgroundColor: '#4ECC5E',
+                                            height: 24,
+                                            width: 24,
+                                            borderRadius: 12,
+                                            alignItems: 'center'
+                                        }}>
+                                            <Text style={{color: 'white'}}>{item.totalMessage}</Text>
+                                        </View>
                                     </View>
                                 </TouchableOpacity>
+                                <View style={{borderBottomWidth: 1, borderBottomColor: '#A8AAAB'}}/>
                                 </>
                             )
                         }}
                     />
                 </SafeAreaView>
+                <TouchableOpacity style={{paddingRight: 24, marginTop: 20}}>
+                    <View style={styles.addChat}>
+                        <Image
+                            style={styles.avatarPen}
+                            source={require('./asset/pencil.png')}
+                        />
+                    </View>
+                </TouchableOpacity>
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    header: {
+        height: 50,
+        backgroundColor: '#517DA2',
+        marginTop: 30,
+        justifyContent: 'center'
+    },
+    subHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    subContentHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    avatarDrawwer: {
+        height: 12,
+        width: 18
+    },
+    titleName: {
+        color: 'white',
+        fontSize: 18,
+        paddingLeft: 20
+    },
+    messageContainer: {
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        paddingTop: 27,
+        paddingHorizontal: 24,
+        paddingVertical: 24,
+        justifyContent: 'space-between'
+    },
+    nameMessage: {
+        paddingLeft: 15
+    },
+    avatarPen: {
+        height: 18,
+        width: 18
+    },
+    addChat: {
+        height: 45,
+        width: 45,
+        borderRadius: 45 / 2,
+        backgroundColor: '#4EA4EA',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'flex-end'
+    }
+})
