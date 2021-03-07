cc094892def68e1d18ab0c0b1824ba84e0d558fe
aminriqky <aminriqky@gmail.com>
2021-03-07 11:56:03 +0700

    Tugas14

--- Tugas 10 - React Native Intro/SanberApp/App.js
+++ Tugas 10 - React Native Intro/SanberApp/App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './Tugas/Tugas13/LoginScreen';
 import RegisScreen from './Tugas/Tugas13/RegisScreen';
 import AboutScreen from './Tugas/Tugas13/AboutScreen';
+import RestApi from './Tugas/Tugas14/RestApi';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="RestApi" component={RestApi}/>
         <Stack.Screen name="LoginScreen" component={LoginScreen}/>
         <Stack.Screen name="RegisScreen" component={RegisScreen}/>
         <Stack.Screen name="AboutScreen" component={AboutScreen}/>
